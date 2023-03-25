a12b11c2ff650ce18f9d6da723aa6784ce5d1594
CEpicwav <leondorner@gmx.de>
2023-03-25 19:57:02 +0100

    Changes

---
+++ A001_zufallsgedicht/zufallsgedicht.js
@@ -0,0 +1,26 @@
+"use strict";
+var zufallsgedicht;
+(function (zufallsgedicht) {
+    let subjects;
+    let predicates;
+    let objects;
+    subjects = ["Harry", "Hermine", "Hagrid", "Tom Riddle", "Snape", "Dumbledore"];
+    predicates = ["hasst", "liebt", "verzaubert", "studiert", "bewundert", "zerstoert"];
+    objects = ["Zaubertraenke", "deine Mom", "Dementoren", "den Zeitumkehrer", "das Spaghettimonster", "seinen Vater"];
+    console.log(subjects, predicates, objects);
+    for (let index = subjects.length; index >= 2; index--) {
+        console.log(getVerse(subjects, predicates, objects));
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        let verse = "";
+        let verseZahl;
+        verseZahl = Math.floor(Math.random() * _subjects.length);
+        let versSubjekt = _subjects.splice(verseZahl, 1);
+        verseZahl = Math.floor(Math.random() * _predicates.length);
+        let versPredikat = _predicates.splice(verseZahl, 1);
+        verseZahl = Math.floor(Math.random() * _objects.length);
+        let versObjekt = _objects.splice(verseZahl, 1);
+        verse = versSubjekt + " " + versPredikat + " " + versObjekt;
+        return verse;
+    }
+})(zufallsgedicht || (zufallsgedicht = {}));
---
+++ A001_zufallsgedicht/zufallsgedicht.ts
@@ -0,0 +1,50 @@
+namespace zufallsgedicht {
+
+    let subjects: string[];
+    let predicates: string[];
+    let objects: string[];
+
+    subjects = ["Harry", "Hermine", "Hagrid", "Tom Riddle", "Snape", "Dumbledore"];
+    predicates = ["hasst",  "liebt", "verzaubert", "studiert", "bewundert", "zerstoert"];
+    objects = ["Zaubertraenke", "deine Mom", "Dementoren", "den Zeitumkehrer", "das Spaghettimonster", "seinen Vater"];
+
+
+    console.log(subjects, predicates, objects);
+
+    for (let index: number = subjects.length; index >=2; index--) {
+
+        console.log(getVerse(subjects, predicates, objects));
+
+    }
+
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+
+        let verse: string = "";
+
+        let verseZahl: number;
+
+        verseZahl = Math.floor(Math.random() * _subjects.length);
+        let versSubjekt: string[] = _subjects.splice(verseZahl, 1);
+
+        verseZahl = Math.floor(Math.random() * _predicates.length);
+        let versPredikat: string[] = _predicates.splice(verseZahl, 1);
+
+        verseZahl = Math.floor(Math.random() * _objects.length);
+        let versObjekt: string[] = _objects.splice(verseZahl, 1);
+
+        verse = versSubjekt + " " + versPredikat + " " + versObjekt;
+
+        return verse;
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
